b61828ef7cb61c512b269bd8e62c84f029e3de31
Nabeel Hingun <nabeel126@berkeley.edu>
2022-01-29 04:37:43 +0000

    update

--- run_attack.sh
+++ run_attack.sh
@@ -19,15 +19,86 @@
 # --task train \
 # --apply_patch
 
+# CUDA_VISIBLE_DEVICES=0 python val_attack_synthetic.py \
+# --img 1280 \
+# --batch-size 16 \
+# --data mtsd.yaml \
+# --weights /data/chawin/adv-patch-bench/yolov5/runs/train/exp2/weights/best.pt \
+# --exist-ok \
+# --workers 8 \
+# --task train \
+# --save_exp_metrics 
+
+# CUDA_VISIBLE_DEVICES=0 python val_attack_synthetic.py \
+# --img 1280 \
+# --batch-size 16 \
+# --data mtsd.yaml \
+# --weights /data/chawin/adv-patch-bench/yolov5/runs/train/exp2/weights/best.pt \
+# --exist-ok \
+# --workers 8 \
+# --task train \
+# --save_exp_metrics \
+# --apply_patch 
+
+# CUDA_VISIBLE_DEVICES=0 python val_attack_synthetic.py \
+# --img 1280 \
+# --batch-size 16 \
+# --data mtsd.yaml \
+# --weights /data/chawin/adv-patch-bench/yolov5/runs/train/exp2/weights/best.pt \
+# --exist-ok \
+# --workers 8 \
+# --task train \
+# --save_exp_metrics \
+# --apply_patch \
+# --random_patch
+
+# CUDA_VISIBLE_DEVICES=0 python val_attack_synthetic.py \
+# --img 1280 \
+# --batch-size 16 \
+# --data mapillary_vistas.yaml \
+# --weights /data/chawin/adv-patch-bench/yolov5/runs/train/exp2/weights/best.pt \
+# --exist-ok \
+# --workers 8 \
+# --task train \
+# --save_exp_metrics 
+
 CUDA_VISIBLE_DEVICES=0 python val_attack_synthetic.py \
 --img 1280 \
 --batch-size 16 \
 --data mapillary_vistas.yaml \
---weights /data/chawin/adv-patch-bench/yolov5/runs/train/exp/weights/best.pt \
+--weights /data/chawin/adv-patch-bench/yolov5/runs/train/exp2/weights/best.pt \
 --exist-ok \
 --workers 8 \
 --task train \
---apply_patch
+--save_exp_metrics \
+--apply_patch 
+
+# CUDA_VISIBLE_DEVICES=0 python val_attack_synthetic.py \
+# --img 1280 \
+# --batch-size 16 \
+# --data mapillary_vistas.yaml \
+# --weights /data/chawin/adv-patch-bench/yolov5/runs/train/exp2/weights/best.pt \
+# --exist-ok \
+# --workers 8 \
+# --task train \
+# --save_exp_metrics \
+# --apply_patch \
+# --random_patch
+
+# CUDA_VISIBLE_DEVICES=0 python val_attack_synthetic.py \
+# --img 1280 \
+# --batch-size 16 \
+# --data mapillary_vistas.yaml \
+# --weights /data/chawin/adv-patch-bench/yolov5/runs/train/exp2/weights/best.pt \
+# --exist-ok \
+# --workers 8 \
+# --task train \
+# --apply_patch \
+# --random_patch \
+# --save_exp_metrics
+
+# --weights /data/chawin/adv-patch-bench/yolov5/runs/train/exp/weights/best.pt \
+
 
 # CUDA_VISIBLE_DEVICES=0 python val.py \
 # --img 1280 \
