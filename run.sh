57371ae0b71845687a0ee45f61b034fb4efe2658
Nabeel Hingun <nabeel126@berkeley.edu>
2022-02-08 06:50:11 +0000

    update

--- run.sh
+++ run.sh
@@ -21,7 +21,7 @@
 #     --experiment clf
 #     # --evaluate
 
-CUDA_VISIBLE_DEVICES=0 python example_transforms.py \
+CUDA_VISIBLE_DEVICES=1 python fix_use_polygon_transforms.py \
     --seed 0 \
     --full-precision \
     --batch-size 256 \
@@ -31,3 +31,14 @@ CUDA_VISIBLE_DEVICES=0 python example_transforms.py \
     --output-dir /data/chawin/adv-patch-bench/results/ \
     --num-classes 12 \
     --resume /data/chawin/adv-patch-bench/results/4/checkpoint_best.pt
+
+# CUDA_VISIBLE_DEVICES=1 python example_transforms.py \
+#     --seed 0 \
+#     --full-precision \
+#     --batch-size 256 \
+#     --data /data/shared/mtsd_v2_fully_annotated/cropped_signs_v6/ \
+#     --dataset mtsd \
+#     --arch resnet18 \
+#     --output-dir /data/chawin/adv-patch-bench/results/ \
+#     --num-classes 12 \
+#     --resume /data/chawin/adv-patch-bench/results/4/checkpoint_best.pt
