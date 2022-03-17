1d96a4b64fe59599835b8790946dc530322144bc
Ubuntu <chawins@2v100.gs5th2ywebwejbzntrvjb51sud.xx.internal.cloudapp.net>
2022-03-17 04:36:30 +0000

    before merge

--- run_attack_chawins1.sh
+++ run_attack_chawins1.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 PATCH_NAME=14_synthetic_10x10
 EXP=10
+GPU=0
 
-# CUDA_VISIBLE_DEVICES=0 python -u generate_adv_patch.py \
+# CUDA_VISIBLE_DEVICES=$GPU python -u generate_adv_patch.py \
+#     --device $GPU \
 #     --seed 0 \
 #     --data mapillary_vistas.yaml \
 #     --weights /data/shared/adv-patch-bench/yolov5/runs/train/exp3/weights/best.pt \
@@ -19,23 +21,15 @@ EXP=10
 #     --imgsz 2560 \
 #     --padded_imgsz 1952,2592
 
-CUDA_VISIBLE_DEVICES=0 python -u val_attack_synthetic.py \
-    --imgsz 2560 \
-    --padded_imgsz 1952,2592 \
-    --batch-size 2 \
+CUDA_VISIBLE_DEVICES=$GPU python -u val_attack_synthetic.py \
     --data mapillary_vistas.yaml \
-    --weights /data/shared/adv-patch-bench/yolov5/runs/train/exp3/weights/best.pt \
-    --exist-ok \
-    --workers 6 \
-    --task train \
-    --save-exp-metrics \
-    --interp bicubic \
-    --attack-type load \
+    --exist-ok --workers 6 --task train --save-exp-metrics \
     --metrics-confidence-threshold 0.359 \
     --adv-patch-path ./runs/val/exp$EXP/$PATCH_NAME.pkl \
     --tgt-csv-filepath mapillary_vistas_final_merged.csv \
-    --attack-config-path attack_config.yaml \
-    --obj-class 14 \
-    --plot-class-examples 14 \
-    --name $PATCH_NAME
+    --attack-config-path attack_config.yaml --name $PATCH_NAME \
+    --weights /data/shared/adv-patch-bench/yolov5/runs/train/exp3/weights/best.pt \
+    --obj-class 14 --plot-class-examples 14 --syn-obj-path attack_assets/octagon-915.0.png \
+    --imgsz 2560 --interp bicubic --padded_imgsz 1952,2592 --batch-size 2 \
+    --attack-type load --synthetic-eval --debug
     # --adv-patch-path ./runs/val/exp2/stop_sign_10x10.pkl \
