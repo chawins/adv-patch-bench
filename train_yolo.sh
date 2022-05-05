f170349ce573438b320a7543d0f6c49a072b0417
Chawin Sitawarin <chawins@berkeley.edu>
2022-05-05 03:44:33 +0000

    azure

--- train_yolo.sh
+++ train_yolo.sh
@@ -20,7 +20,7 @@ CUDA_VISIBLE_DEVICES=$GPU torchrun \
     --standalone --nnodes=1 --max_restarts 0 --nproc_per_node $NUM_GPU \
     train_yolov5.py \
     --hyp yolov5/data/hyps/hyp.scratch.yaml --weights yolov5/yolov5s.pt --exist-ok \
-    --data yolov5/data/mtsd_no_color.yaml --img 1000 --batch 200 \
+    --data yolov5/data/mtsd_no_color.yaml --img 1000 --batch 128 \
     --workers 20 --device $GPU --save-period 5 --epochs 100 --sync-bn
 # NOTE: --cache uses a lot of memeory (~80G)
 
