a141a88ad202be987192d860d15f6fee2806e8b4
Chawin Sitawarin <chawins@berkeley.edu>
2022-05-11 19:35:33 +0000

    add sign  counts in hparams

--- adv_patch_bench/utils/detectron/custom_sampler.py
+++ adv_patch_bench/utils/detectron/custom_sampler.py
@@ -1,7 +1,7 @@
 import itertools
 import math
 from collections import defaultdict
-
+import random
 import torch
 from detectron2.utils import comm
 from torch.utils.data.sampler import Sampler
@@ -122,3 +122,43 @@ class RepeatFactorTrainingSampler(Sampler):
                 yield from indices[randperm].tolist()
             else:
                 yield from indices.tolist()
+
+
+class ShuffleInferenceSampler(Sampler):
+    """
+    Produce indices for inference across all workers.
+    Inference needs to run on the __exact__ set of samples,
+    therefore when the total number of samples is not divisible by the number of workers,
+    this sampler produces different number of samples on different workers.
+    """
+
+    def __init__(self, size: int):
+        """
+        Args:
+            size (int): the total number of data of the underlying dataset to sample from
+        """
+        self._size = size
+        assert size > 0
+        self._rank = comm.get_rank()
+        self._world_size = comm.get_world_size()
+        self._local_indices = self._get_local_indices(size, self._world_size, self._rank)
+
+
+    @staticmethod
+    def _get_local_indices(total_size, world_size, rank):
+        shard_size = total_size // world_size
+        left = total_size % world_size
+        shard_sizes = [shard_size + int(r < left) for r in range(world_size)]
+
+        begin = sum(shard_sizes[:rank])
+        end = min(sum(shard_sizes[: rank + 1]), total_size)
+        indices = list(range(begin, end))
+        # EDIT: shuffle indices
+        random.shuffle(indices)
+        return indices
+
+    def __iter__(self):
+        yield from self._local_indices
+
+    def __len__(self):
+        return len(self._local_indices)
