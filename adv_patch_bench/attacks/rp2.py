fix bugs; add resize_and_center and coerce_rank; decouple patch and sign sizes

adv_patch_bench/attacks/rp2.py
@@ -29,7 +29,7 @@ class RP2AttackModule(DetectorAttackModule):
         super(RP2AttackModule, self).__init__(
             attack_config, core_model, loss_fn, norm, eps, **kwargs)
         self.input_size = attack_config['input_size']
-        
+
         rp2_config = attack_config['rp2']
         self.num_steps = rp2_config['num_steps']
         self.step_size = rp2_config['step_size']
@@ -59,7 +59,7 @@ class RP2AttackModule(DetectorAttackModule):
             # self.post_nms_topk = {True: 5000, False: 5000}
             self.nms_thresh = self.nms_thresh_orig
             self.post_nms_topk = self.post_nms_topk_orig
-        
+
         # Use change of variable on delta with alpha and beta.
         # Mostly used with per-sign attack.
         self.use_var_change_ab = 'var_change_ab' in self.attack_mode
@@ -91,16 +91,16 @@ class RP2AttackModule(DetectorAttackModule):
             30, translate=(0.45, 0.45), p=augment_prob, return_transform=True,
             resample=self.interp)  # DEBUG
         self.mask_transforms = K.RandomAffine(
-            30, translate=(0.45, 0.45), p=augment_prob, 
+            30, translate=(0.45, 0.45), p=augment_prob,
             resample=Resample.NEAREST)
         self.jitter_transform = K.ColorJitter(
             brightness=0.3, contrast=0.3, p=augment_prob)
-        
+
         # Define EoT augmentation for attacking real signs
         # Transforms patch and background when attacking real signs
         self.real_transform = {
             'tf_patch': K.RandomAffine(
-                15, translate=(0.1, 0.1), scale=(0.9, 1.1), p=augment_prob, 
+                15, translate=(0.1, 0.1), scale=(0.9, 1.1), p=augment_prob,
                 resample=self.interp)
         }
         # Background should move very little because gt annotation is fixed
@@ -156,12 +156,14 @@ class RP2AttackModule(DetectorAttackModule):
         conf, labels = conf.max(-1)
         if obj_class is not None:
             loss = 0
+            # Loop over EoT batch
             for c, l in zip(conf, labels):
                 c_l = c[l == obj_class]
                 if c_l.size(0) > 0:
                     # Select prediction from box with max confidence and ignore
                     # ones with already low confidence
-                    loss += c_l.max().clamp_min(self.min_conf)
+                    # loss += c_l.max().clamp_min(self.min_conf)
+                    loss += c_l.clamp_min(self.min_conf).sum()  # FIXME
             loss /= self.num_eot
         else:
             loss = conf.max(1)[0].clamp_min(self.min_conf).mean()
@@ -175,7 +177,7 @@ class RP2AttackModule(DetectorAttackModule):
         # NOTE: IoU threshold for ROI is 0.5 and for RPN is 0.7
         _, target_labels, target_logits, obj_logits = get_targets(
             self.core_model, metadata, device=self.core_model.device,
-            iou_thres=self.detectron_iou_thres, score_thres=self.min_conf, 
+            iou_thres=self.detectron_iou_thres, score_thres=self.min_conf,
             use_correct_only=False)
 
         # DEBUG
@@ -281,7 +283,7 @@ class RP2AttackModule(DetectorAttackModule):
         backgrounds.detach_()
         _, _, obj_height, obj_width = mask_to_box(obj_mask)
         all_bg_idx = np.arange(len(backgrounds))
-        
+
         obj_mask_eot = obj_mask.expand(self.num_eot, -1, -1, -1)
         patch_mask_eot = patch_mask.expand(self.num_eot, -1, -1, -1)
         obj_eot = obj.expand(self.num_eot, -1, -1, -1)
@@ -291,7 +293,7 @@ class RP2AttackModule(DetectorAttackModule):
 
         for _ in range(self.num_restarts):
             # Initialize adversarial perturbation
-            z_delta = torch.zeros((1, 3, self.patch_dim, self.patch_dim), 
+            z_delta = torch.zeros((1, 3, self.patch_dim, self.patch_dim),
                                   device=device, dtype=dtype, requires_grad=True)
             z_delta.uniform_(0, 1)
 
@@ -344,12 +346,12 @@ class RP2AttackModule(DetectorAttackModule):
                         new_instances.gt_boxes = Boxes(box)
                         new_instances.gt_classes = torch.tensor([[obj_class]])
                         m['instances'] = new_instances
-                
+
                 with torch.enable_grad():
                     z_delta.requires_grad_()
                     delta = self._to_model_space(z_delta, 0, 1)
                     delta_padded = resize_and_center(
-                        delta, self.input_size, (obj_height, obj_width), 
+                        delta, self.input_size, (obj_height, obj_width),
                         is_binary=False)
                     delta_eot = delta_padded.expand(self.num_eot, -1, -1, -1)
                     delta_eot = self.obj_transforms.apply_transform(
@@ -357,7 +359,7 @@ class RP2AttackModule(DetectorAttackModule):
                     adv_obj = p_mask * delta_eot + (1 - p_mask) * adv_obj
                     # Augment sign and patch with relighting
                     adv_obj = self.jitter_transform(adv_obj)
-                    
+
                     # Apply sign on background
                     adv_img = o_mask * adv_obj + (1 - o_mask) * bgs
                     adv_img = adv_img.clamp(0, 1)
@@ -450,7 +452,7 @@ class RP2AttackModule(DetectorAttackModule):
             tf_data.append(data_i)
 
         all_bg_idx = np.arange(len(tf_data_temp))
-        backgrounds = torch.cat([obj[0].unsqueeze(0) for obj in objs], 
+        backgrounds = torch.cat([obj[0].unsqueeze(0) for obj in objs],
                                 dim=0).to(device)
 
         for _ in range(self.num_restarts):
@@ -517,7 +519,6 @@ class RP2AttackModule(DetectorAttackModule):
         # Return worst-case perturbed input logits
         self.core_model.train(mode)
         return delta.detach()
-
 
     def _step_opt(self, z_delta, opt):
         if self.optimizer == 'pgd':

adv_patch_bench/utils/image.py
@@ -192,8 +192,10 @@ def resize_and_center(obj:torch.Tensor,
 
     if img_size is not None:
         # left, top, right, bottom
-        left = (img_size[1] - obj_size[1]) // 2
-        top = (img_size[0] - obj_size[0]) // 2
+        # left = (img_size[1] - obj_size[1]) // 2
+        # top = (img_size[0] - obj_size[0]) // 2
+        left = torch.div(img_size[1] - obj_size[1], 2, rounding_mode='trunc')
+        top = torch.div(img_size[0] - obj_size[0], 2, rounding_mode='trunc')
         pad_size = [
             left,  # left
             top,  # top

gen_patch_yolo.py
@@ -23,7 +23,7 @@ from torch.nn import DataParallel
 from adv_patch_bench.attacks.rp2 import RP2AttackModule
 from adv_patch_bench.utils.argparse import (eval_args_parser,
                                             setup_yolo_test_args)
-from adv_patch_bench.utils.image import get_obj_width
+from adv_patch_bench.utils.image import get_obj_width, resize_and_center
 from gen_mask import generate_mask
 from hparams import LABEL_LIST, MAPILLARY_IMG_COUNTS_DICT, OTHER_SIGN_CLASS
 from yolov5.models.common import DetectMultiBackend
@@ -127,28 +127,20 @@ def generate_adv_patch(
         obj_mask = torch.from_numpy(obj_numpy[:, :, -1] == 1).float().unsqueeze(0)
         obj = torch.from_numpy(obj_numpy[:, :, :-1]).float().permute(2, 0, 1)
         # Resize object to the specify size and pad obj and masks to image size
-        pad_size = [(img_size[1] - obj_size[1]) // 2,
-                    (img_size[0] - obj_size[0]) // 2,
-                    (img_size[1] - obj_size[1]) // 2 + obj_size[1] % 2,
-                    (img_size[0] - obj_size[0]) // 2 + obj_size[0] % 2]  # left, top, right, bottom
-        obj = T.resize(obj, obj_size, antialias=True)
-        obj = T.pad(obj, pad_size)
-
-        obj_mask = T.resize(obj_mask, obj_size, interpolation=T.InterpolationMode.NEAREST)
-        obj_mask = T.pad(obj_mask, pad_size)
-
+        obj = resize_and_center(obj, img_size, obj_size, is_binary=False)
+        obj_mask = resize_and_center(
+            obj_mask, img_size, obj_size, is_binary=True)
         patch_mask = patch_mask.unsqueeze(dim=0)
-        patch_mask_ = T.resize(patch_mask, obj_size, interpolation=T.InterpolationMode.NEAREST)
-
-        patch_mask_ = T.pad(patch_mask_, pad_size)
+        patch_mask_padded = resize_and_center(
+            patch_mask, img_size, obj_size, is_binary=True)
 
         print(f'=> Start attacking...')
         with torch.enable_grad():
             adv_patch = attack.attack(obj.to(device),
                                       obj_mask.to(device),
-                                      patch_mask_.to(device),
+                                      patch_mask_padded.to(device),
                                       backgrounds.to(device),
-                                      obj_size=obj_size)
+                                      obj_class=obj_class)
 
         if save_images:
             torchvision.utils.save_image(obj, join(save_dir, 'obj.png'))
@@ -163,30 +155,22 @@ def generate_adv_patch(
 
         attack_images = []
         print('=> Collecting background images...')
-        
-        # filename_debug = '6obAf9CRQh_dBFHPAIiRFQ.jpg'
-        filename_debug = 'HwBonTfmkfIOANA1O7B2OQ.jpg'
 
         from tqdm import tqdm
         for batch_i, (im, targets, paths, shapes) in tqdm(enumerate(dataloader)):
-            # import pdb
-            # pdb.set_trace()
-
-            # DEBUG
-            # if f'/datadrive/nab_126/data/mapillary_vistas/no_color/combined/images/{filename_debug}' not in paths:
-                # continue
 
             for image_i, path in enumerate(paths):
                 filename = path.split('/')[-1]
 
                 # DEBUG
+                # filename_debug = 'HwBonTfmkfIOANA1O7B2OQ.jpg'
                 # if filename != filename_debug:
                 #     continue
                 # tmp_df = pd.read_csv('results_per_label_errors.csv')
                 # if filename not in tmp_df['filename'].values:
                 #     continue
                 # print('attack_images', len(attack_images))
-                
+
                 img_df = df[df['filename'] == filename]
                 if len(img_df) == 0:
                     continue
@@ -233,8 +217,10 @@ def generate_adv_patch(
     adv_patch = adv_patch[0].detach().cpu().float()
 
     if save_images:
-        torchvision.utils.save_image(patch_mask, join(save_dir, 'patch_mask.png'))
-        torchvision.utils.save_image(adv_patch, join(save_dir, 'adversarial_patch.png'))
+        torchvision.utils.save_image(patch_mask,
+                                     join(save_dir, 'patch_mask.png'))
+        torchvision.utils.save_image(adv_patch,
+                                     join(save_dir, 'adversarial_patch.png'))
 
     return adv_patch
 
@@ -272,9 +258,9 @@ def main(
     class_names = LABEL_LIST[args.dataset]
 
     # Set up directories
-    save_dir = increment_path(Path(project) / name, exist_ok=exist_ok)  # increment run
+    # save_dir = increment_path(Path(project) / name, exist_ok=exist_ok)  # increment run
     # (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
-    save_dir = save_dir / class_names[obj_class]
+    save_dir = Path(project) / name / class_names[obj_class]
     os.makedirs(save_dir, exist_ok=True)
 
     # Load model (YOLO)

test_yolo.py
@@ -137,36 +137,36 @@ def populate_default_metric(lbl_, min_area, filename, other_class_label):
 
 
 @torch.no_grad()
-def run(args,
-        data,
-        weights=None,  # model.pt path(s)
-        batch_size=32,  # batch size
-        imgsz=640,  # inference size (pixels)
-        conf_thres=0.001,  # confidence threshold
-        iou_thres=0.6,  # NMS IoU threshold
-        task='val',  # train, val, test, speed or study
-        device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
-        workers=8,  # max dataloader workers (per RANK in DDP mode)
-        single_cls=False,  # treat as single-class dataset
-        augment=False,  # augmented inference
-        verbose=False,  # verbose output
-        save_txt=False,  # save results to *.txt
-        save_hybrid=False,  # save label+prediction hybrid results to *.txt
-        save_conf=False,  # save confidences in --save-txt labels
-        save_json=False,  # save a COCO-JSON results file
-        project=ROOT / 'runs/val',  # save to project/name
-        name='exp',  # save to project/name
-        exist_ok=False,  # existing project/name ok, do not increment
-        half=True,  # use FP16 half-precision inference
-        dnn=False,  # use OpenCV DNN for ONNX inference
-        dataloader=None,
-        save_dir=Path(''),
-        plots=True,
-        callbacks=Callbacks(),
-        compute_loss=None,
-        **kwargs,
-        ):
-
+def run(
+    args,
+    data,
+    weights=None,  # model.pt path(s)
+    batch_size=32,  # batch size
+    imgsz=640,  # inference size (pixels)
+    conf_thres=0.001,  # confidence threshold
+    iou_thres=0.6,  # NMS IoU threshold
+    task='val',  # train, val, test, speed or study
+    device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+    workers=8,  # max dataloader workers (per RANK in DDP mode)
+    single_cls=False,  # treat as single-class dataset
+    augment=False,  # augmented inference
+    verbose=False,  # verbose output
+    save_txt=False,  # save results to *.txt
+    save_hybrid=False,  # save label+prediction hybrid results to *.txt
+    save_conf=False,  # save confidences in --save-txt labels
+    save_json=False,  # save a COCO-JSON results file
+    project=ROOT / 'runs/val',  # save to project/name
+    name='exp',  # save to project/name
+    exist_ok=False,  # existing project/name ok, do not increment
+    half=True,  # use FP16 half-precision inference
+    dnn=False,  # use OpenCV DNN for ONNX inference
+    dataloader=None,
+    save_dir=Path(''),
+    plots=True,
+    callbacks=Callbacks(),
+    compute_loss=None,
+    **kwargs,
+):
     # Load our new args
     attack_type = args.attack_type
     use_attack = args.attack_type != 'none'
@@ -231,9 +231,9 @@ def run(args,
         model = DetectMultiBackend(weights, device=device, dnn=dnn)
         stride, pt, jit, engine = model.stride, model.pt, model.jit, model.engine
         # check image size
-        imgsz = check_img_size(imgsz, s=stride)  
+        imgsz = check_img_size(imgsz, s=stride)
         # half precision only supported by PyTorch on CUDA
-        half &= (pt or jit or engine) and device.type != 'cpu'  
+        half &= (pt or jit or engine) and device.type != 'cpu'
         if pt or jit:
             model.model.half() if half else model.model.float()
         elif engine:
@@ -272,7 +272,7 @@ def run(args,
     elif model_name == 'yolor':
         img = torch.zeros((1, 3, imgsz, imgsz), device=device)  # init img
         # run once to warmup
-        _ = model(img.half() if half else img) if device.type != 'cpu' else None  
+        _ = model(img.half() if half else img) if device.type != 'cpu' else None
 
     dataloader = create_dataloader(data[task], imgsz, batch_size, stride,
                                    single_cls, pad=pad, rect=pt,
@@ -306,15 +306,6 @@ def run(args,
     #     f = os.path.join(save_dir, 'adversarial_patch.png')
     #     torchvision.utils.save_image(demo_patch, f)
 
-    if synthetic:
-        # Prepare evaluation with synthetic signs
-        # syn_data: syn_obj, obj_mask, obj_transforms, mask_transforms, syn_sign_class
-        syn_data = prep_synthetic_eval(args, img_size, names, device=device)
-        syn_sign_class = syn_data[-1]
-        nc += 1
-        adv_patch = None
-        patch_mask = None
-
     if use_attack:
         # Prepare attack data
         adv_patch_dir = os.path.join(
@@ -323,6 +314,16 @@ def run(args,
         args.adv_patch_path = adv_patch_path
         df, adv_patch, patch_mask, patch_loc = prep_attack(
             args, img_size, device)
+
+    if synthetic:
+        # Prepare evaluation with synthetic signs
+        # syn_data: syn_obj, obj_mask, obj_transforms, mask_transforms, syn_sign_class
+        syn_data = prep_synthetic_eval(args, img_size, names, device=device)
+        names[nc] = 'synthetic'
+        syn_sign_class = syn_data[-1]
+        nc += 1
+        # adv_patch = None
+        # patch_mask = None
 
     # Initialize attack
     if attack_type == 'per-sign':
@@ -381,7 +382,6 @@ def run(args,
             break
         # ======================= BEGIN: apply patch ======================== #
         for image_i, path in enumerate(paths):
-            # print()
             if use_attack and not synthetic:
                 filename = path.split('/')[-1]
                 img_df = df[df['filename'] == filename]
@@ -418,21 +418,17 @@ def run(args,
                                 obj_class=adv_sign_class)[0]
 
                     # # Transform and apply patch on the image. `im` has range [0, 255]
-                    # print(im[image_i].max())
-                    img_normalized, warped_patch_num_pixels = transform_and_apply_patch(
+                    img, warped_patch_num_pixels = transform_and_apply_patch(
                         im[image_i].to(device), adv_patch.to(device),
                         patch_mask, patch_loc, predicted_class, row, img_data,
-                        transform=args.patch_transform,
-                        use_relight=not args.no_patch_relight, interp=args.interp)
-                    im[image_i] = img_normalized
-                    # print(im[image_i].max())
-                    # num_patches_applied_to_image += 1
+                        args.transform_mode, interp=args.interp,
+                        use_relight=not args.no_patch_relight)
+                    im[image_i] = img
                     total_num_patches += 1
                     patch_size_df_filenames.append(row['filename'])
                     patch_size_df_object_ids.append(row['object_id'])
                     patch_size_df_num_pixels.append(warped_patch_num_pixels)
 
-                
                 # set targets[6] to #patches_applied_to_image
                 # targets[targets[:, 0] == image_i, 6] = num_patches_applied_to_image
 
@@ -456,10 +452,10 @@ def run(args,
         # Inference
         if model_name == 'yolov5':
             # inference, loss outputs
-            out, train_out = model(im, augment=augment, val=True)  
+            out, train_out = model(im, augment=augment, val=True)
         elif model_name == 'yolor':
             # inference and training outputs
-            out, train_out = model(im, augment=augment)  
+            out, train_out = model(im, augment=augment)
 
         dt[1] += time_sync() - t2
 
@@ -542,8 +538,8 @@ def run(args,
                         # [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
                         x1 = 0.9 * lbl[1] if 0.9 * lbl[1] > 5 else -20
                         y1 = 0.9 * lbl[2] if 0.9 * lbl[2] > 5 else -20
-                        if (prd[0] >= x1 and prd[1] >= y1 and 
-                            prd[2] <= 1.1 * lbl[3] and prd[3] <= 1.1 * lbl[4]):
+                        if (prd[0] >= x1 and prd[1] >= y1 and
+                                prd[2] <= 1.1 * lbl[3] and prd[3] <= 1.1 * lbl[4]):
                             if prd[5] == adv_sign_class:
                                 predn[pi, 5] = syn_sign_class
                                 pred[pi, 5] = syn_sign_class
@@ -562,7 +558,7 @@ def run(args,
                 # `label_idx`: idx of object in `labels`
                 # `pred_idx`: idx of object in `predn`
 
-                _, iou_matches, _ = process_batch(predn, labelsn, iouv, 
+                _, iou_matches, _ = process_batch(predn, labelsn, iouv,
                                                   match_on_iou_only=True)
                 iou_matches = []
                 correct, matches, iou = process_batch(
@@ -596,13 +592,15 @@ def run(args,
 
             for lbl_index, lbl_ in enumerate(labels):
                 if annotated_signs_only and not synthetic:
-                    annotation_row = annotation_df[(annotation_df['filename'] == filename) &
-                                                   (annotation_df['object_id'] == int(lbl_[5]))]
+                    annotation_row = annotation_df[
+                        (annotation_df['filename'] == filename) &
+                        (annotation_df['object_id'] == int(lbl_[5]))
+                    ]
                     assert len(annotation_row) <= 1
                     # Ignore label if either (1) not annotated or (2) was used
                     # to generate patch attack.
                     is_annotated = len(annotation_row) > 0
-                    used_to_gen_path = (filename in filename_list and 
+                    used_to_gen_path = (filename in filename_list and
                                         not args.run_only_img_txt)
                     if not is_annotated or used_to_gen_path:
                         # Ignore by setting label to 'other' class which is
@@ -686,8 +684,9 @@ def run(args,
                 false_positives_filenames.append(filename)
 
             # if unmatched and small, the prediction should be removed
-            pred_index_to_keep = np.logical_and(pred_index_to_keep, ~np.logical_and(
-                unmatched_preds_index, small_preds_index))
+            pred_index_to_keep = np.logical_and(
+                pred_index_to_keep, ~np.logical_and(
+                    unmatched_preds_index, small_preds_index))
 
             correct = correct[pred_index_to_keep]
             pred = pred[pred_index_to_keep]
@@ -756,7 +755,9 @@ def run(args,
     #                            END: Main eval loop                          #
     # ======================================================================= #
 
-    patch_size_df_data = {'filename': patch_size_df_filenames, 'object_id': patch_size_df_object_ids, 'patch_num_pixels': patch_size_df_num_pixels}
+    patch_size_df_data = {
+        'filename': patch_size_df_filenames, 'object_id': patch_size_df_object_ids,
+        'patch_num_pixels': patch_size_df_num_pixels}
     patch_size_df = pd.DataFrame.from_dict(patch_size_df_data)
     metrics_per_label_df = metrics_per_label_df.merge(right=patch_size_df, on=['filename', 'object_id'], how='left')
 
@@ -774,12 +775,14 @@ def run(args,
                 # labels
                 f = save_dir_class / f'image{i}_labels.jpg'  # labels
                 targets[:, 0] = 0
-                plot_images(im, targets, [path], f, names, labels=True)
+                plot_images(im, targets, [path], f, names,
+                            max_size=1920, labels=True)
 
                 # predictions
                 f = save_dir_class / f'image{i}_pred.jpg'  # labels
                 out[:, 0] = 0
-                plot_images(im, out, [path], f, names, labels=False)
+                plot_images(im, out, [path], f, names,
+                            max_size=1920, labels=False)
 
     # Compute metrics
     stats = [np.concatenate(x, 0) for x in zip(*stats)]  # to numpy